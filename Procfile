93180189700d4ebdfca3b633f8e3463eab3012d3
Alvar Maciel <alvarmaciel@gmail.com>
2022-03-06 16:56:35 -0300

    env y proc update

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 web: gunicorn humanoBackendTest.wsgi --log-file -
-release: python manage.py migrate
+release: python manage.py makemigrations --noimput
+release: python manage.py collectstatic --noimput
+release: python manage.py migrate --noimput
--- humanoBackendTest/settings.py
+++ humanoBackendTest/settings.py
@@ -9,14 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django-heroku
 from pathlib import Path
 import os
-import environ
-
-env = environ.Env()
-
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,25 +86,13 @@ WSGI_APPLICATION = "humanoBackendTest.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env(DATABASE_NAME),
-        "USER": env(DATABAE_USER),
-        "PASSWORD": env(DATABASE_PASSWORD),
-        "HOST": env(DATABASE_HOST),
-        "PORT": env(DATBASE_PORT),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -155,3 +138,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_heroku.settings(locals())
