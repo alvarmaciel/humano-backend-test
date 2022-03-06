e46acd7de6de69c176163448506f0886563da3a6
Alvar Maciel <alvarmaciel@gmail.com>
2022-03-06 15:58:47 -0300

    envirement variables added

--- humanoBackendTest/settings.py
+++ humanoBackendTest/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gfa6_9y%da!ot%bx@@bf&n9jd*^*@xvmx7rfhbot0x5h%5uxdt'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
