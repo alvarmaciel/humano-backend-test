bd0305bb33a00283502f233a69e34bcca1c6732d
Alvar Maciel <alvarmaciel@gmail.com>
2022-03-06 16:57:29 -0300

    env y proc update

--- humanoBackendTest/settings.py
+++ humanoBackendTest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django-heroku
+import django_heroku
 from pathlib import Path
 import os
 
@@ -87,10 +87,9 @@ WSGI_APPLICATION = "humanoBackendTest.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
